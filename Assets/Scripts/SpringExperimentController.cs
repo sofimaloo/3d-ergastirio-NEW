95cd5e96b464cd79fb16fbadc87d4fbb138b6343
Tsakiris212 <Kostastsakires060a@gmail.com>
2025-07-06 14:03:43 +0300

    συνεχιζουμε

---
+++ Assets/Scripts/SpringExperimentController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpringExperimentController : MonoBehaviour
+{
+    public Rigidbody mass;
+    public Vector3 nudgeVelocity = new Vector3(0, -1f, 0);
+
+    public Toggle springToggle; // ➕ Νέο πεδίο για το UI
+
+    public void StartSpringExperiment()
+    {
+        if (mass != null)
+        {
+            mass.velocity = nudgeVelocity;
+            Debug.Log("Ξεκίνησε το πείραμα ελατηρίου!");
+        }
+
+        // ➕ Τικάρισμα του toggle όταν ξεκινάει
+        if (springToggle != null)
+        {
+            springToggle.isOn = true;
+        }
+    }
+}
